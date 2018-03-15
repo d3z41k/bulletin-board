87dcb52aa49b57a9d761ca7cc57c4b71907715db
Denn Zhokhoff <d3z41k@gmail.com>
2018-03-15 20:16:31 +0200

    Add user role

--- database/migrations/2018_03_15_172844_add_user_role.php
+++ database/migrations/2018_03_15_172844_add_user_role.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserVerification extends Migration
+class AddUserRole extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,11 @@ class AddUserVerification extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('status', 16);
-            $table->string('verify_token')->nullable()->unique();
+            $table->string('role', 16);
         });
 
         DB::table('users')->update([
-            'status' => 'active',
+            'role' => 'user',
         ]);
     }
 
@@ -31,8 +30,7 @@ class AddUserVerification extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('status');
-            $table->dropColumn('verify_token');
+            $table->dropColumn('role');
         });
     }
 }
