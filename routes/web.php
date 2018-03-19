26533cf82989d513251ca0f747b013ec26bc335b
Denn Zhokhoff <d3z41k@gmail.com>
2018-03-19 21:35:37 +0200

    Add categories

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,14 @@ Route::group(
         Route::resource('users', 'UsersController');
         Route::post('/users/{user}/verify', 'UsersController@verify')->name('users.verify');
         Route::resource('regions', 'RegionController');
+
+        Route::group([
+            'prefix' => 'adverts',
+            'as' => 'adverts.',
+            'namespace' => 'Adverts'
+        ],
+        function () {
+            Route::resource('categories', 'CategoryController');
+        });
     }
 );
