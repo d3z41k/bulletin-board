2404f58c2f68069f33d745197e1dcf433dc26012
Denn Zhokhoff <d3z41k@gmial.com>
2018-03-23 22:07:04 +0200

    Add cabinet adverts page

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::group(
                     Route::post('/phone/auth', 'PhoneController@auth')->name('phone.auth');
                 }
             );
+
+            Route::resource('adverts', 'Adverts\AdvertController');
         }
 );
 
