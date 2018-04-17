842c1ee7f30122b2e71ea48fe892be6982e1f99e
d3z41k <d3z41k@gmail.com>
2018-04-17 15:18:36 +0200

    Add API home controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'Api\HomeController@home');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
