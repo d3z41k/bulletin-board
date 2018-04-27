84b4314ea1b04959b7add9e27e692ce66ca87dec
d3z41k <d3z41k@gmial.com>
2018-04-27 12:53:39 +0200

    Added moderation passed event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,31 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
+use App\Events\Advert\ModerationPassed;
+use App\Listeners\Advert\ModerationPassedListener;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        ModerationPassed::class => [
+            ModerationPassedListener::class,
         ],
     ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-
-        //
-    }
 }
